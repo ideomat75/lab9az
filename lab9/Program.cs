6ee92667933927aebb412eabd241df00ad6c5a86
ideomat75 <90716549+ideomat75@users.noreply.github.com>
2022-11-19 00:51:10 +0300

    Add files via upload

---
+++ lab9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+using System.IO;
+using System.Globalization;
+
+namespace Csh_lab09
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            long time = DateTimeOffset.Now.ToUnixTimeSeconds();
+            long timeYearAgo = time - 31556926;
+            List<string> tickers = new List<string>();
+            List<double> Avers = new List<double>();
+            object locker = new object();
+            using (StreamReader reader = new StreamReader("ticker.txt"))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    tickers.Add(line);
+                }
+            }
+            foreach (var elem in tickers)
+            {
+                using (var client = new HttpClient())
+                {
+                    var content = await client.GetStringAsync($"https://query1.finance.yahoo.com/v7/finance/download/{elem}?period1={timeYearAgo}&period2={time}&interval=1d&events=history&includeAdjustedClose=true");
+                    string[] text = content.Split('\n');
+                    var counter = Task.Factory.StartNew(() =>
+                    {
+                        double sum = 0;
+                        double average = 0;
+                        for(int i = 1; i < text.Length; ++i)
+                        {
+                            sum += (double.Parse(text[i].Split(',')[2]) + double.Parse(text[i].Split(',')[3])) / 2;
+                        }
+                        average = sum / text.Length;
+                        Console.WriteLine(average);
+                        Avers.Add(average);
+                    });
+                }
+            }
+            using (StreamWriter write = new StreamWriter("tickersAnd.txt"))
+            {
+                for (int i = 0; i < Avers.Count; ++i)
+                {
+                    lock (locker)
+                    {
+                        write.WriteLine($"{tickers[i]}: {Avers[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ weather/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+
+namespace Cs_lab06
+{
+    class Program
+    {
+
+        struct Weather
+        {
+            public string Country;
+            public string Name;
+            public double Temp;
+            public string Description;
+            public Weather(string _country, string _name, double _temp, string _desc)
+            {
+                Country = _country;
+                Name = _name;
+                Temp = _temp;
+                Description = _desc;
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            var random = new System.Random();
+            double lon = random.NextDouble() * 360 - 180;
+            double lat = random.NextDouble() * 180 - 90;
+            string api = "69d628518916cfeb2075778ed593e6c6";
+            var client = new HttpClient();
+            var content = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}");
+
+            List<Weather> weathers = new List<Weather>(50);
+
+            while (weathers.Count() < 50)
+            {
+                lon = random.NextDouble() * 360 - 180;
+                lat = random.NextDouble() * 180 - 90;
+                client = new HttpClient();
+                content = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}");
+
+                Regex reCountry = new Regex("(?<=\"country\":\")[^\"]+(?=\")");
+                Regex reName = new Regex("(?<=\"name\":\")[^\"]+(?=\")");
+                Regex reTemp = new Regex("(?<=\"temp\":)[^:]+(?=,)");
+                Regex reDesc = new Regex("(?<=\"description\":\")[^\"]+(?=\")");
+
+                MatchCollection cMatches = reCountry.Matches(content);
+                MatchCollection nMatches = reName.Matches(content);
+                MatchCollection tMatches = reTemp.Matches(content);
+                MatchCollection dMatches = reDesc.Matches(content);
+
+                if (cMatches.Count != 0)
+                {
+                    if (nMatches.Count != 0)
+                    {
+                        Weather tmpWeathear = new Weather(cMatches[0].Value, nMatches[0].Value, Convert.ToDouble(tMatches[0].Value, System.Globalization.CultureInfo.InvariantCulture) - 273, dMatches[0].Value);
+                        weathers.Add(tmpWeathear);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            foreach (Weather obj in weathers)
+            {
+                Console.WriteLine($"Сountry: {obj.Country}  Name: {obj.Name} Temp: {obj.Temp}  Desc: {obj.Description}");
+            }
+        }
+    }
+}
